82ea619073e097b3f85270cee646dc3eb99f59b4
Adam Weis <aweis@andrew.cmu.edu>
2012-05-27 11:25:50 -0700

    Finished the basic signup/login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,27 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Logged in!"
+    else
+      flash.now.alert = "Email or password is invalid."
+      render "new"
+    end
+  end
+
+  #logging out
+  def destroy
+    session[:user_id] = nil
+    
+    # This resets the entire session, but Ryan Bates says setting
+    # :user_id = nil would suffice
+    # reset_session
+    
+    redirect_to root_url, notice: "Logged out!"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Ghc::Application.routes.draw do
    
   root :to => 'schools#index'
-  
-  get "sessions/new"
 
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :sessions
   resources :users
 
   resources :invitations
